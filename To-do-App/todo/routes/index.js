5a4816b192343d04bef38ea210f9d70407dea110
saransappa <saran.sappa@gmail.com>
2021-03-06 15:24:54 +0530

    added new feature: edit task

--- To-do-App/todo/routes/index.js
+++ To-do-App/todo/routes/index.js
@@ -4,12 +4,11 @@ const request = require('request');
 const MongoClient = require('mongodb').MongoClient
 /*
 
-
-		ENTER YOUR MONGODB ATLAS URI below
+ ENTER YOUR MONGO DB ATLAS URI BELOW
 
 
 */
-const uri = <your MongoDB Atlas URI>;
+const uri = "";         
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -135,6 +134,64 @@ router.get('/get_tasks',function(req,res,next){
         res.send(result);
       });  
 	
+	})
+});
+
+/* GET page for editing tasks */
+router.get('/edit_tasks',function(req,res,next){
+	var html = "<!DOCTYPE html><html><head><title>Edit Task</title></head><body><center><h1>Delete Task</h1><textarea name=\"new_task\" form=\"usrform\" placeholder=\"Enter your edited task here\"></textarea><form action = \"http://localhost:3000/edit_task_details\" method = \"POST\" id=\"usrform\">Select task to be edited:<select name=\"old_task\"> <option value='default'></option>";
+  	var json_file;
+	request.get({
+		  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+		  url:     'http://localhost:3000/get_tasks',
+		}, function(error, response, body){
+			if(!error){
+		  		//console.log(body);
+				json_file = JSON.parse(body);
+				//console.log(json_file);
+		  		var s = "<option value=";
+				var t = "</option>"
+
+		  		for(var i=0;i<json_file.length;i++){
+
+					var k = s+ JSON.stringify(json_file[i].task) + ">"+ json_file[i].task+t;
+					html+=k;
+
+		  		}
+		  		html+= "</select><br/><br/><input type = \"submit\" value = \"Edit\"></form></center></body></html>";
+		  		res.send(html);
+			}
+			else{
+				console.log(error);
+			}
+	});
+});
+
+/* POST for editing tasks */
+router.post('/edit_task_details',function(req,res,next){
+	var query = {task: req.body.old_task}; 
+	var new_task = req.body.new_task;
+	console.log(query);
+	var z; 
+	MongoClient.connect(uri, (err, client) => {
+	  if (err) return console.error(err)
+  	  console.log('Connected to Tickets Database');
+  	  const db = client.db('todo');
+  	  const detailsCollection = db.collection('tasks');
+  	  detailsCollection.find(query).toArray(function(err, result) {  
+		if (err) console.log(err);  
+		else {
+			var newvalue = {$set: {task:new_task}};
+			detailsCollection.updateOne(query, newvalue,function(err_,result_){
+				if(!err_){
+					res.writeHead(301,
+						{Location: 'http://localhost:3000/' }
+					);
+					res.end();
+				}
+			});
+		}
+		});  
 	})
 });
 
